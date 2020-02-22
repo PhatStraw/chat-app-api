15123be9408ffebcebe2c343a9794b0134040b6d
kevinsims1 <kevinsims1@outlook.com>
2020-02-21 21:07:25 -0800

    configured mongoose/mongo

--- index.js
+++ index.js
@@ -1,11 +1,43 @@
-require('dotenv').config()
-var app = require('express')();
-var http = require('http').createServer(app);
+require("dotenv").config();
+var app = require("express")();
+var session = require("express-session");
+var grant = require("grant-express");
+var http = require("http").createServer(app);
+var morgan = require("morgan");
+var bp = require("body-parser");
 
-app.get('/', function(req, res){
-  res.send('<h1>Hello world</h1>');
+app.use(
+  bp.urlencoded({
+    extended: true
+  })
+);
+
+app.use(bp.json());
+
+app.use(morgan("dev"));
+
+app.use(session({ secret: "grant" }));
+// mount grant
+
+app.use(
+  grant({
+    "github": {
+        "key": process.env.GH_ID, "secret": process.env.GH_SECRET,
+        "scope": ["public_repo"],
+        "callback": "/connect/github/callback"
+      }
+  })
+);
+
+app.get("/", function(req, res) {
+  res.send("<h1>Hello world</h1>");
 });
 
-http.listen(process.env.PORT, function(){
+app.post("/connect/github/callback", function(req, res) {
+    console.log(req.query);
+    res.send(200)
+  });
+
+http.listen(process.env.PORT, function() {
   console.log(`listening on *:${process.env.PORT}`);
 });
---
+++ schema.js
@@ -0,0 +1,28 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var userSchema = new Schema({
+    name:  {type: String, required: true}, 
+    email: {type: String, required: true, unique: true},
+    gh_id: {type: String, required: true, unique: true},
+  });
+
+
+var messageSchema = new Schema({
+from:  {type: mongoose.SchemaTypes.ObjectId, ref: "user",required: true}, 
+room: {type: mongoose.SchemaTypes.ObjectId, ref: "room", required: true},
+message: {type: String, required: true},
+});
+
+var chatRoomSchema = new Schema({
+    created_by:  {type: mongoose.SchemaTypes.ObjectId, ref: "user",required: true},
+    members: [{type: mongoose.SchemaTypes.ObjectId, ref: "user",required: true}],
+    name: String
+  });
+
+
+module.exports = {
+    User: mongoose.model('user', userSchema),
+    Message: mongoose.model('message', messageSchema),
+    Chat: mongoose.model('chat', chatRoomSchema)
+}
